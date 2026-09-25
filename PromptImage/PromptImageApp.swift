8ddec69ddf148b227501ff69871a9dde000f6cc3
Ilya Diyakov <tumanezhikov@yandex.ru>
2026-09-25 15:34:17 +0300

    Set up the iOS project

---
+++ PromptImage/PromptImageApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct PromptImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PromptImageTests/PromptImageTests.swift
@@ -0,0 +1,4 @@
+import Testing
+@testable import PromptImage
+
+// Add behavioral tests here when the app has logic to exercise.
